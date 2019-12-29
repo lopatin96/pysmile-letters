19b949bb8d12cf3a959819f6d445345c815137b5
Vladyslav Lopatin <ilopatin3@gmail.com>
2019-12-30 00:47:30 +0100

    fixed some bugs

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ class LettersClassifier:
         self.net = None
         self.letter = None
         self.evidences = {
-            'spam': ['free', 'Satisfied', 'Accept credit cards', 'Acceptance', 'access', 'Accordingly'],
+            'spam': ['free', 'satisfied', 'accept credit cards', 'acceptance', 'won', 'access', 'online', 'bargain',
+                'best price', 'call now', 'call free', 'cash bonus', 'chance', 'click', 'credit', 'free access',
+                'free money', 'gift certificate', 'limited', 'lifetime'],
             'love': ['love you', 'loved you', 'for always', 'the words to describe', 'how much you', 'let my heart',
-                     'express', 'do that for', 'thinking of', 'want you', 'share', 'excited', 'feeling', 'hot',
-                     'romantic', 'honey', 'every moment', 'every second', 'heart', 'forever', 'happy', 'my dear',
-                     'unforgettable', 'meet']
+                'express', 'do that for', 'thinking of', 'want you', 'share', 'excited', 'feeling', 'hot',
+                'romantic']
         }
 
     def set_net(self, net_path):
@@ -28,8 +29,8 @@ class LettersClassifier:
                 if evidence.strip().lower() in self.letter:
                     self.net.set_evidence(f'{k}_{evidence.strip().replace(" ", "_").lower()}', 'y')
                     continue
-                self.net.set_evidence(f'{k}_{evidence.strip().replace(" ", "_").lower()}', 'n')  # ?
-        self.net.update_beliefs()
+                self.net.set_evidence(f'{k}_{evidence.strip().replace(" ", "_").lower()}', 'n') # ?
+                self.net.update_beliefs()
 
     def get_node_value(self, node):
         self._do()
@@ -37,27 +38,21 @@ class LettersClassifier:
 
 
 if __name__ == '__main__':
-    net_name = 'LettersClassifierNet'
+    net_name = 'LettersClassifierNet1'
     net_path = 'C:\\Users\\SuperPC\\PycharmProjects\\sc-letters\\'
 
     lettersClassifier = LettersClassifier()
     lettersClassifier.set_net(net_path + net_name + '.xdsl')
 
-    # love letter
-    lettersClassifier.set_letter('Honey,nThere are not enough words to express what you mean to me. I cherish every moment and every new memory. Every second that we share is seared into my mind and in my heart forever. You\'ve left an imprint deep upon my soul. I love you more than you could ever imagine. Forever yours,')
-    print('Love letter')
-    beliefs = lettersClassifier.get_node_value('spam_RESULT')
-    print('spam:', beliefs)
-    beliefs = lettersClassifier.get_node_value('love_RESULT')
-    print('love:', beliefs)
-    print('---------')
+    letters = {
+        'spam': 'Avangar Technologies announces the beginning of a new unprecendented global employment campaign. reviser yeller winers butchery twenties Due to company\'s exploding growth Avangar is expanding business to the European region. During last employment campaign over 1500 people worldwide took part in Avangar\'s business and more than half of them are currently employed by the company. And now we are offering you ne more opportunity to earn extra money working with Avangar Technologies. druggists blame classy gentry Aladdin',
+        'love': 'Honey,nThere are not enough words to express what you mean to me. I cherish every moment and every new memory. Every second that we share is seared into my mind and in my heart forever. You\'ve left an imprint deep upon my soul. I love you more than you could ever imagine. Forever yours,'
+    }
 
-    # random text
-    lettersClassifier.set_letter('Wordclouds.com is a free online word cloud generator and tag cloud creator. Wordclouds.com works on your PC, Tablet or smartphone. Paste text, upload a document or open an URL to automatically generate a word- or tag cloud. Customize your cloud with shapes, themes, colors and fonts. You can also edit the word list, cloud size and gap size. Wordclouds.com can also generate clickable word clouds with links (image map). When you are satisfied with the result, save the image and share it online.')
-    print('Random text')
-    beliefs = lettersClassifier.get_node_value('spam_RESULT')
-    print('spam:', beliefs)
-    beliefs = lettersClassifier.get_node_value('love_RESULT')
-    print('love:', beliefs)
-    print('---------')
-
+    for type, letter in letters.items():
+        lettersClassifier.set_letter(letter)
+        print(f'___ {type} ___')
+        for type in letters.keys():
+                beliefs = lettersClassifier.get_node_value(f'{type}_RESULT')
+                print(f'| {type}: {round(beliefs[0] * 100, 2)}%')
+        print('_______\n')
